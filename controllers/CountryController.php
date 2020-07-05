20e7659825a14d49177892faa55ac97cbbf67f95
Shakirzhan <samarbaev99@mail.ru>
2020-07-05 20:37:26 +0300

    training

--- controllers/CountryController.php
+++ controllers/CountryController.php
@@ -9,21 +9,4 @@ class CountryController extends ActiveController
 {
     public $modelClass = 'app\models\country';
 
-    public function actions()
-    {
-        $actions = [
-            'index' => [
-                'class' => \app\models\IndexAction::class,
-                'modelClass' => $this->modelClass,
-                'checkAccess' => [$this, 'checkAccess'],
-            ],
-        ];
-
-        return array_merge(parent::actions(), $actions);
-    }
-
-    public function actionIndex()
-    {
-        return 'new111111111111';
-    }
 }
