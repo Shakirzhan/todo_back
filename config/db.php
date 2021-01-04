cd560d98e51efe4cd88bfbdfef516c5d24179f55
Shakirzhan <samarbaev99@mail.ru>
2021-01-04 22:09:07 +0300

    added attributes

--- config/db.php
+++ config/db.php
@@ -6,6 +6,7 @@ return [
     'username' => 'root',
     'password' => 'dfAD1t6hvm20.',
     'charset' => 'utf8',
+    'attributes' => [PDO::ATTR_CASE => PDO::CASE_LOWER],
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
