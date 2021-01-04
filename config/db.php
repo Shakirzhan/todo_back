f6803cf3135cd4c09a3792a22445ee738d56d0f4
Shakirzhan <samarbaev99@mail.ru>
2021-01-04 11:27:47 +0300

    config

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=127.0.0.1:3306;dbname=hack',
     'username' => 'root',
-    'password' => '',
+    'password' => 'dfAD1t6hvm20.',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
